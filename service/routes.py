2a1ce8694f450a86d3a815c88d43fb04543a1470
mega-wojciech <wojciech14703@gmail.com>
2026-01-29 07:29:25 -0500

    added update account

--- service/routes.py
+++ service/routes.py
@@ -88,8 +88,16 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """Updates an Account"""
+    app.logger.info("Request to update Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] not found.")
+    account.deserialize(request.get_json())
+    account.update()
+    return jsonify(account.serialize()), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
